0e8f3b98f94a48fc2484e327c617e9bb01d2a651
JamesDa1 <JamesDa.kodehode@gmail.com>
2022-08-19 12:12:45 +0200

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,4 @@
 import React from "react"
-import projectImage from "../images/calculator.png"
-import { AiFillGithub } from "react-icons/ai"
-import { data } from "./data"
 import { ProjectList } from "../components/Projects"
 
 const Projects = () => {
