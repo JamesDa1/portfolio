321234ad185532aa4ae91329171d3c044502b8ce
JamesDa1 <JamesDa.kodehode@gmail.com>
2022-08-22 14:58:33 +0200

    Added fake-store to projects

--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -11,12 +11,12 @@ export const projects = [
   },
   {
     id: 2,
-    title: "React-Calculator using useState",
+    title: "Fake Store",
     description:
-      "En enkel kalkulator laget i React. Bruker useState/useEffect for funksjonalitet og styled-components for CSS",
-    tech: ["React", "styled components"],
-    img: require("../../images/calculator.png"),
-    sourceCode: "https://github.com/JamesDa1/react-calculator-useState",
-    liveLink: "https://jamesda1.github.io/react-calculator-useState/",
+      "En web store laget i React. Jeg brukte useContext og useReducer for handlelisten, axios for å hente informasjon fra API. ",
+    tech: ["React", "styled components", "useContext", "useReducer"],
+    img: require("../../images/fake-store.png"),
+    sourceCode: "https://github.com/JamesDa1/fake-store-v2",
+    liveLink: "https://jamesda1.github.io/fake-store-v2/",
   },
 ]
